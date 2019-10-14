43b7abea708d4695cf7af2386ad6a254bfbdb455
Amaury Neury <amaury.neury@gmail.com>
2019-10-14 13:10:00 +0200

    app prout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { FilmsListComponent } from './films-list/films-list.component';
 import { HighlightDirective } from './highlight.directive';
+import { MenuComponent } from './menu/menu.component';
+import { ROUTES } from './app.route';
 
 
 @NgModule({
@@ -17,12 +19,15 @@ import { HighlightDirective } from './highlight.directive';
     UserProfileComponent,
     FilmsListComponent,
     HighlightDirective,
+    MenuComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
